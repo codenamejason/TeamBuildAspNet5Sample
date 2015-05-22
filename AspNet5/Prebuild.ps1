e61870b14e839a2ff88556e376880ceb8962f584
Chris Patterson <chris@pattersonplace.org>
2015-05-22 12:06:19 -0400

    redirecting stderr to stdout

--- AspNet5/Prebuild.ps1
+++ AspNet5/Prebuild.ps1
@@ -9,5 +9,5 @@ $dnxVersion = $globalJson.sdk.version
 # install DNX
 & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
 
-# run DNU restore on all project.json files in the src folder
-Get-ChildItem -Path $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName }
+# run DNU restore on all project.json files in the src folder # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
+Get-ChildItem -Path $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
