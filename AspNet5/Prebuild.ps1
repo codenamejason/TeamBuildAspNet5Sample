3e6c7a0a47b6554a7b2a7756d6c29422312cf06d
Chris Patterson <chrispat@microsoft.com>
2015-05-22 14:00:36 -0400

    fixing minor issues in prebuild and publish scripts

--- AspNet5/Prebuild.ps1
+++ AspNet5/Prebuild.ps1
@@ -1,13 +1,21 @@
-
 # bootstrap DNVM into this session.
 &{$Branch='dev';iex ((new-object net.webclient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/dev/dnvminstall.ps1'))}
 
 # load up the global.json so we can find the DNX version
-$globalJson = Get-Content -Path $PSScriptRoot\global.json -Raw | ConvertFrom-Json
-$dnxVersion = $globalJson.sdk.version
+$globalJson = Get-Content -Path $PSScriptRoot\global.json -Raw -ErrorAction Ignore | ConvertFrom-Json -ErrorAction Ignore
+
+if($globalJson)
+{
+    $dnxVersion = $globalJson.sdk.version
+}
+else
+{
+    Write-Warning "Unable to locate global.json to determine using 'latest'"
+    $dnxVersion = "latest"
+}
 
 # install DNX
 & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
 
 # run DNU restore on all project.json files in the src folder
-Get-ChildItem -Path $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName }
+Get-ChildItem -LiteralPath $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName }
