ab6d1f52cb6fe6a8769d3f0baeba8f68f87f3f97
Chris Patterson <chrispat@microsoft.com>
2015-05-26 09:31:21 -0400

    Fixing merge issues

--- AspNet5/Prebuild.ps1
+++ AspNet5/Prebuild.ps1
@@ -18,4 +18,3 @@ else
 & $env:USERPROFILE\.dnx\bin\dnvm install $dnxVersion -Persistent
 
 # run DNU restore on all project.json files in the src folder # run DNU restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Get-ChildItem -Path $PSScriptRoot\src -Filter project.json -Recurse | ForEach-Object { & dnu restore $_.FullName 2>1 }
